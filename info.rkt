3515ca1dbd2b7c7abce21125e68288caa75f6204
Alexis King <lexi.lambda@gmail.com>
2017-03-17 22:10:23 -0700

    Remove old planet references and add package dependencies

--- info.rkt
+++ info.rkt
@@ -1,16 +1,19 @@
 #lang info
 
-(define scribblings '(("scribblings/unlib.scrbl" (multi-page))))
-
 (define collection "unlib")
 
 (define deps
-  '())
+  '("base"
+    "compatibility-lib"
+    "gregor-lib"
+    "preprocessor"
+    "rackunit-lib"
+    "scheme-lib"
+    "scribble-lib"
+    "srfi-lib"
+    "srfi-lite-lib"))
 (define build-deps
-  '())
+  '("racket-doc"
+    "srfi-doc"))
 
-(define compile-omit-paths
-  '("autoplanet.rkt"
-    "build.rkt"
-    "planet"
-    "planetdev"))
+(define scribblings '(("scribblings/unlib.scrbl" (multi-page))))
